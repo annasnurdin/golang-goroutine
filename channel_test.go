7acc6932a39261cb8f50e9147c0c4b2bbc780ae7
annasnurdin <anasnurdin02@gmail.com>
2026-01-09 11:36:12 +0700

    default select

--- channel_test.go
+++ channel_test.go
@@ -113,3 +113,28 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelect(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go Response(channel1)
+	go Response(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1: ", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2: ", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
